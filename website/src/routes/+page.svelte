c85db4390cb1556cb72e143d3c7ef695287c72f3
BetterinBinary <152219933+BetterinBinary@users.noreply.github.com>
2023-11-27 16:03:30 -0500

    skeleton
    initial skeleton

---
+++ website/src/routes/+page.svelte
@@ -0,0 +1,64 @@
+
+<script>
+    import { onMount } from "svelte";
+
+    let message = "loading"
+    let timeout = 1000;
+
+    onMount(() => {
+    });
+
+    $: if (message.length > 10) {
+        message = "loading";
+    }
+
+    $: {
+        if (message.length <= 10){
+            setTimeout(() => {
+                message += '.';
+            }, timeout);
+        }
+    }
+
+</script>
+
+<div class=main>
+    <h1>Welcome to Primo's</h1>
+    <span class=msg><h3>{message}</h3></span>
+</div>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+
+<style>
+    html {
+        font-family: sans-serif;
+        line-height: 1.5;
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        background-color : #38446B;
+    }
+    .main{
+        background-color: #253a80;
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        margin-left: -8px;
+        margin-top: -8px;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .main h1{
+        color: #e3b212;
+        font-size: 3rem;
+        margin: 0;
+    }
+
+    .msg{
+        width: 4rem;
+        text-align: left;
+        color: #e3b212;
+    }
+
+</style>
